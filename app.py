046e3968423796c59192c7766ebe14067cf03bd6
jimthomas1997 <jmthms833@gmail.com>
2020-09-25 13:48:24 +0530

    added images and posts.html

--- app.py
+++ app.py
@@ -1,29 +1,37 @@
 from flask import Flask,render_template
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
+import sqlite3
 
 app=Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///posts.db'
-db=SQLAlchemy(app)
+# app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///posts.db'
+# db = SQLAlchemy(app)
 
-class BlogPost(db.Model):
-    id=db.Column(db.Integer,primary_key=True)
-    title=db.Column(db.String(100),nullable=False)
-    content=db.Column(db.Text,nullable=False)
-    author=db.Column(db.String(30),nullable=False,default='N/A')
-    date_posted=db.column(db.DateTime,nullable=False,default=datetime.utcnow)
+# class ItemPost(db.Model):
+#     id = db.Column(db.Integer,primary_key=True)
+#     title=db.Column(db.String(100))
+#     content=db.Column(db.Text)
+#     seller=db.Column(db.String(30))
+#     date_posted=db.column(db.DateTime)
 
-    def __repr__(self):
-        return 'Blog Post' + str(self.id)
+#     def __repr__(self):
+#         return 'Blog Post' + str(self.id)
 
 all_posts=[
-    {
+    {     
+          'author':'jim',
           'title':'Post 1',
-          'content':'content of post 1',  
+          'content':'content of post 1',
+          'date_posted':'April 20,2020', 
+          'price':'₹10,999'
+
     },
     {
+          'author':'kevin',
           'title':'Post 2',
-          'content':'content of post 2',  
+          'content':'content of post 2', 
+          'date_posted':'April 21,2020',
+          'price':'₹11,999'
     }
 
 ]
